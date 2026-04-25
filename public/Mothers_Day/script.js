00c0c95617b73a072e2e9cef42e20ff6471e4ec2
NoleStites <nole.stites@gmail.com>
2026-04-25 18:20:43 -0500

    Mother's Day site begun

---
+++ public/Mothers_Day/script.js
@@ -0,0 +1,14 @@
+var step = 0;
+
+const content = [
+    {"type": "img", "src": "[file_path]", "description": "[content]"},
+    {"type": "audio", "src": "[file_path]"},
+]
+
+document.getElementById("rightBtn").addEventListener("click", function() {
+    console.log("right");
+});
+
+document.getElementById("leftBtn").addEventListener("click", function() {
+    console.log("left");
+});
